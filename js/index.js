fc2876220b3f84b218ef60d9085272eb02a66677
ziyodilloxon <ziyodilloxon8@gmail.com>
2024-03-31 12:37:43 +0500

    uy ishi bajarildi

---
+++ js/index.js
@@ -0,0 +1,50 @@
+// 1 misol
+// class Car {
+//   constructor(brand, year) {
+//     this.brand = brand;
+//     this.year = year;
+//   }
+// }
+// let car1 = new Car("Toyota", 2022);
+// let car2 = new Car("BMW", 2015);
+// let car3 = new Car("Bugatti", 2019);
+// console.log(car1);
+// console.log(car2);
+// console.log(car3);
+
+// 2 misol
+// class ElectroCar extends Car {
+//   constructor(brand, year, batteryLife) {
+//     super(brand, year);
+//     this.batteryLife = batteryLife;
+//   }
+// }
+// let electroCar1 = new ElectroCar("Tesla", 2023, 300);
+// let electroCar2 = new ElectroCar("Nissan", 2021, 250);
+// let electroCar3 = new ElectroCar("Chevrolet", 2024, 280);
+// console.log(electroCar1);
+// console.log(electroCar2);
+// console.log(electroCar2);
+
+// 3 misol
+// class Car {
+//   constructor(brand, year) {
+//     this.brand = brand;
+//     this.year = year;
+//   }
+//   displayInfo() {
+//     console.log(`Brand: ${this.brand}, Year: ${this.year}`);
+//   }
+// }
+// class ElectroCar extends Car {
+//   constructor(brand, year, batteryLife) {
+//     super(brand, year);
+//     this.batteryLife = batteryLife;
+//   }
+// }
+// let electroCar1 = new ElectroCar("Tesla", 2023, 300);
+// let electroCar2 = new ElectroCar("Nissan", 2021, 250);
+// let electroCar3 = new ElectroCar("Chevrolet", 2024, 280);
+// electroCar1.displayInfo();
+// electroCar2.displayInfo();
+// electroCar3.displayInfo();
